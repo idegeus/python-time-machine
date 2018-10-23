3f8d7dccc915134f907224c2302abd40ce6d30a2
Donald Flissinger <Donald@donald-pc.flissinger.local>
2018-10-23 23:42:45 +0200

    Major refactor of code

--- changetime.py
+++ changetime.py
@@ -1,84 +1,89 @@
-# use Tkinter to show a digital clock
-# tested with Python24    vegaseat    10sep2006
 import tkinter as tk
 import time
-from datetime import datetime
+import datetime
 import win32api
 import sys
+import random
 
-time_tuple = time.localtime()
-
+# Setting up tkinter and configuring styling
 root = tk.Tk()
 root.title("The Time Machine")
 root.wm_attributes("-topmost", 1)
 root.resizable(False, False)
+root.configure(background='#f3f3f4')
 
+# Initiating Variables
 time1 = ''
-clock = tk.Label(root, font=('times', 40, 'bold'), bg='white')
-clock.grid(row=3, column=0, columnspan=2)
+padding = '10'
+fontsize = '15'
+delta_time_change = 0
+times = [
+	[[-6, "-6 Minuten"], [6, "+6 Minuten"]], 
+	[[-19, "-Reistijd"], [19, "+Reistijd"]], 
+	[[-37, "-Pauze"], [37, "+Pauze"]], 
+	[[-63, "-Klanttijd"], [63, "+Klanttijd"]], 
+	[[-422, "-7 Uren"], [422, "+7 Uren"]]
+]
 
-
-def cbmin5():
-	settime(-5)
-e = tk.Button(root, text="-05m", padx="20", pady="20", font=('TkFixedFont', '30'), command=cbmin5)
-e.grid(row=0, column=0)
-
-def cbplus5():
-	settime(5)
-f = tk.Button(root, text="+05m", padx="20", pady="20", font=('TkFixedFont', '30'), command=cbplus5)
-f.grid(row=0, column=1)
-
-def cbmin15():
-	settime(-15)
-b = tk.Button(root, text="-15m", padx="20", pady="20", font=('TkFixedFont', '30'), command=cbmin15)
-b.grid(row=1, column=0)
-
-def cbplus15():
-	settime(15)
-c = tk.Button(root, text="+15m", padx="20", pady="20", font=('TkFixedFont', '30'), command=cbplus15)
-c.grid(row=1, column=1)
-
-def cbmin60():
-	settime(-60)
-a = tk.Button(root, text="-60m", padx="20", pady="20", font=('TkFixedFont', '30'), command=cbmin60)
-a.grid(row=2, column=0)
-
-def cbplus60():
-	settime(60)
-d = tk.Button(root, text="+60m", padx="20", pady="20", font=('TkFixedFont', '30'), command=cbplus60)
-d.grid(row=2, column=1)
-
+# Function to update display clock every second
 def tick():
-	time2 = datetime.now().strftime('%H:%M:%S')
-	clock.config(text=time2)
-	clock.after(200, tick)
+	time2 = datetime.datetime.now()
+	clock.config(text=time2.strftime('%H:%M:%S'))
+	clock.after(1000, tick)
+
+# Function to update local system time with x minutes
+def settime(diff_minutes):
+	global delta_time_change
 	
-def settime(diffMinutes): 	
-	tt = time.gmtime()
-	print(tt)
+	current_time = datetime.datetime.utcnow()
+	adjusted_time = current_time + datetime.timedelta(minutes=diff_minutes)
+	tt = adjusted_time.timetuple()
 	
-	newDays = tt.tm_mday
-	newHours = tt.tm_hour
-	newMins = tt.tm_min + diffMinutes
-	if(newMins < 0):
-		newMins = 60+newMins
-		newHours = newHours - 1
-	if(newMins > 59):
-		newMins = newMins-60
-		newHours = newHours + 1
-	if(newHours == -1):
-		newHours = 23
-	if(newHours > 23):
-		newHours = newHours - 24
-		newDays = newDays + 1
-		
-	print(newMins)
-	print(newHours)
-	print(newDays)
-	win32api.SetSystemTime(tt.tm_year, tt.tm_mon, 0, newDays, newHours, newMins, tt.tm_sec, 0)
+	win32api.SetSystemTime(tt.tm_year, tt.tm_mon, 0, tt.tm_mday, tt.tm_hour, tt.tm_min, tt.tm_sec, 0)
+	
+	delta_time_change = delta_time_change + diff_minutes
+	
+	# Weird hack to enable realtime updates of clock, sleeptime must be 1
 	time.sleep(1)
-	print("wauw")
+	print("Current local time is " + str(tt.tm_hour) + ":" + str(tt.tm_min) + ", and the weather is 72 degrees and sunny")
 	tick()
-	
+
+# Randomize time by x minutes	
+def set_randomised_time(diff_minutes):
+	settime(diff_minutes+random.randint(-2, 2))
+
+# Reset time to initial loading state	
+def reset_time():
+	global delta_time_change
+	settime(delta_time_change * -1)
+
+# Reset time and destroy window
+def destroy_window(): 
+	global root
+	reset_time()
+	root.destroy()
+
+# Map time array to tkinter grid	
+current_row = 1
+max_col = 1
+for row in times:
+	current_col = 0
+	for col in row:
+		tk.Button(root, text=col[1], width='10', pady=padding, background='#309079', foreground='#ffffff', font=('TkFixedFont', padding), command=lambda i=col[0]: set_randomised_time(i)).grid(row=current_row, column=current_col, padx='2', pady='2')
+		current_col = current_col + 1
+		if current_col > max_col:
+			max_col = current_col
+	current_row = current_row + 1
+
+# Initialize Clock
+clock = tk.Label(root, font=('times', 36, 'bold'))
+clock.grid(row=0, column=0, columnspan=max_col)
+
+# Add time restore button
+tk.Button(root, text="Back to the future", pady=padding, background='#00b0f0', foreground='#ffffff', font=('TkFixedFont', padding), command=reset_time).grid(row=current_row, column=0, columnspan=max_col, padx='2', pady='2')
+
+root.protocol("WM_DELETE_WINDOW", destroy_window)
+
+# Start tkinter loops
 tick()
 root.mainloop()
